31751aa142cfa28291e7f956b7699372a35968e0
GVellar <98723846+GVellar@users.noreply.github.com>
2022-02-06 00:51:26 -0300

    Add files via upload

--- livro.js
+++ livro.js
@@ -1,6 +1,22 @@
-<script>    
+   
+  var nome = prompt ("Qual seu nome?");
+            alert ("Ola " +nome);
+             var horas = prompt ("Quantas horas voce trabalha por mes?");
+              var  salario = prompt ("Qual seu salario?");
+                var dividir = salario / horas ;
+                    alert ("Voce recebe por hora trabalhada R$" + dividir  );
 
- alert ("Bem vindo");
+            var nasc = prompt ("Em que ano voce nasceu?");
+                var ano = prompt ("Escolha um ano, para saber a idade que voce vai ter?");
+                    var subtracao = nasc - ano;
+                        alert ("Voce completara no ano escolhido a seguinte idade " + subtracao );
+      
+                        alert  ("Saiba a porcentagem do garçom de acordo com valor da sua conta, sendo que a % fixada é de 10%")
+                var valor = prompt ("informe o valor da conta")
+                        var calc = valor * 0.10;
+                        var total = valor = calc;  
+                        alert ("Voce vai pagar ao gorçom um total de:" + total);
+         
+    
 
 
-</script>
